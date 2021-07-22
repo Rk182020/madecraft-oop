6642ed6759fb94bdde60140ec49a31fae86e810b
kharyar <kharyar@hcl.com>
2021-07-22 14:15:55 +0530

    exercises/Smurf.java - improvement for creating only one object for smurf papa.

--- 4. Static/exercises/Smurf.java
+++ 4. Static/exercises/Smurf.java
@@ -3,8 +3,22 @@ package exercises;
 public class Smurf {
 
 	private String name;
+	private static Smurf papaSmurf;
+
 
 	public static Smurf createSmurf(String name) {
+		if(name == "Papa"){
+			/* *
+			 * We have 4 types of Smurf like lady, Papa, left-handy
+			 * right-handy So we need to make sure only 1 Papa smurf 
+			 * object should create not more than 1.
+			 * *
+			 */
+			if(papaSmurf == null)
+				papaSmurf = new Smurf(name);
+			else
+				return papaSmurf;
+		}
 		System.out.println("Creating " + name + " Smurf");
 		return new Smurf(name);
 	}
